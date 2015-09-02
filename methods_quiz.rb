63478b54cd2a707b06f13371936561c819632a28
Tanner May <tannerjmay@gmail.com>
2015-09-02 09:55:25 -0600

    has_teen? is complete

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,7 +1,12 @@
 module MethodsQuiz
 	
 	# TODO - write has_teen?
-	
+	def has_teen?(a, b, c)
+		if a >= 13 && a <= 19 || b >= 13 && b <= 19 || c >= 13 && c <= 19
+			return true
+		end
+		return false
+	end
 	# TODO - write not_string
 	
 	# TODO - write icy_hot?
